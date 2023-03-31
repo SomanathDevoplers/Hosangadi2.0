0bf1ccc7df5aa0c10d8074431a55de828db97705
Krishnanand Shenoy <61490527+Krishnanand136@users.noreply.github.com>
2023-03-31 21:55:04 +0530

    Update structureOfChangableDB .sql

--- runOnMarch31/structureOfChangableDB .sql
+++ runOnMarch31/structureOfChangableDB .sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `somanath2022` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `somanath2022`;
+CREATE DATABASE  IF NOT EXISTS `somanath2023` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `somanath2023`;
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
--- Host: localhost    Database: somanath2022
+-- Host: localhost    Database: somanath2023
 -- ------------------------------------------------------
 -- Server version	8.0.26
 
